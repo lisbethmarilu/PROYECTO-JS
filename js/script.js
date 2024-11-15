3c93fee57d72b34b5d113092d4b6595396fe4327
“lisbehcayot“ <lisbethmarilu@gmail.com>
2024-11-15 11:25:38 -0500

    regreso

--- js/script.js
+++ js/script.js
@@ -6,8 +6,13 @@ const punto = document.querySalectorAll('.grande')
 punto.forEach( (cadaPunto , i)=> {
     punto[i].addEventListener('click',()=> {
         let posicion = i
-        let operacion = posicion * -16.6
+        let operacion = posicion * -50
         
     grande.style.transform = `translateX(${operacion}%)`
+
+    punto.forEach((cadaPunto , i)=>{
+        punto[i].classList.remove('activo')
+    })
+    punto[i].classList.add('activo')
 })
 })
